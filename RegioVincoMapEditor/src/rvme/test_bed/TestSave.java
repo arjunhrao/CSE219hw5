46edeb72d6f82f1ca4b72e7d0b25a12c55cdb60b
Arjun Rao <ravirao@mac-a82066442d0d>
2016-07-10 02:50:24 -0400

    Starting to make the TestSave, although I'm not entirely sure that the package is in the right location.

---
+++ RegioVincoMapEditor/src/rvme/test_bed/TestSave.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rvme.test_bed;
+
+/**
+ *
+ * @author ravirao
+ */
+public class TestSave {
+    
+    /**Have your driver class hard code the creation of all the necessary data values
+     * such that the Andorra map and data could be created. This means everything.
+     * The map data, the images, the background color, the border color and thickness, etc.
+     * Note that your TestSave class would artificially initialize all the data such that
+     * it can then be used by your file manager to save it to /work/Andorra.json, which again,
+     * would be in your own JSON format.
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
