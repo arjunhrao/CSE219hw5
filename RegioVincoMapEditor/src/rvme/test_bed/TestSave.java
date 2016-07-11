02c22066c3ef6e76e9413346e42a59b340a5b891
Arjun Rao <ravirao@mac-a82066442d0d>
2016-07-11 01:42:03 -0400

    Made some changes. Need to load the polygons from the Andorra.json file and save all data fields to a new json file. However, I'm getting nullpointers. This is prob bc of the way I declared a new app but I'm not sure.

--- RegioVincoMapEditor/src/rvme/test_bed/TestSave.java
+++ RegioVincoMapEditor/src/rvme/test_bed/TestSave.java
@@ -5,6 +5,12 @@
  */
 package rvme.test_bed;
 
+import java.io.IOException;
+import javafx.scene.paint.Color;
+import rvme.MapEditorApp;
+import rvme.data.DataManager;
+import rvme.file.FileManager;
+
 /**
  *
  * @author ravirao
@@ -18,9 +24,43 @@ public class TestSave {
      * it can then be used by your file manager to save it to /work/Andorra.json, which again,
      * would be in your own JSON format.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        MapEditorApp app = new MapEditorApp();
+        DataManager dataManager = new DataManager(app);
+        FileManager fileManager = new FileManager();
+        //FileManager fileManager = (FileManager)app.getFileComponent();
+        
+        
+        //create a new JSON file on save
+        //on load, loads THAT Json file
+        //call method in FileManager that does the saving of data / json file, similar to HW1
+        
+        //hardcoding Andorra
+    
+        //step 1: Create Regions
+        //step 2: Import All data into data
+
+        //step 1 - need to access the path to the JSON file - can just note this and use in the datamanager
+        //the datamanager/workspace should be able to access the file and then create the subregions
+        //as it does for the json files from hw2
+        String rawMapPath = "./HW5SampleData/raw_map_data/Andorra.json";
+        //load the data from andorra.json into the polygon list in the datamanager
+        //it's okay that this part isn't exactly 'hard-coded' because who's gonna put all of the andorra points
+        //into this class, amirite?
+        dataManager.getPolygonList().clear();
+        
+        fileManager.loadData(dataManager, rawMapPath);
+        
+        dataManager.setBackgroundColor(Color.rgb(220,110,0));
+        dataManager.setBorderColor(Color.BLACK);
+        dataManager.setBorderThickness(1.0);
+        dataManager.setMapName("Andorra");
+        dataManager.setRawMapPath("./HW5SampleData/raw_map_data/Andorra.json");
+        dataManager.setParentDirectory(rawMapPath);
         
         
         
+        // SAVE IT TO A FILE
+	fileManager.saveData(dataManager, "./HW5SampleData/raw_map_data/Andorra.json");
     }
 }
