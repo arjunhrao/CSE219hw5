a996748b8f53afe806089b5de67bb80c70263ddf
Arjun Rao <ravirao@mac-a82066442d0d>
2016-07-11 07:14:33 -0400

    Starting step 3 - JUnit

---
+++ RegioVincoMapEditor/test/rvme/test_bed/SaveAndLoadTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rvme.test_bed;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ravirao
+ */
+public class SaveAndLoadTest {
+    
+    public SaveAndLoadTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println("* UtilsJUnit4Test: @BeforeClass method");
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+        System.out.println("* UtilsJUnit4Test: @AfterClass method");
+    }
+
+    /**
+     * Test of main method, of class TestSave.
+     */
+    @Test
+    public void testAndorra() throws Exception {
+        
+    }
+    
+    @Test
+    public void testSanMarino() throws Exception {
+        
+    }
+    
+    @Test
+    public void testSlovakia() throws Exception {
+        
+    }
+}
